c3e76b782ba7d3703c0dbc5263f39f75c03d9544
NoFyPablo <jpablo.judge@gmail.com>
2022-03-31 14:06:00 -0500

    Elimine las variables $ant1 y $ant2, por que en el ciclo, se sumaban las ultimas iteraciones e ignoraba las demas.
    agregamos la misma variable a la suma para que sea un valor acumulativo 
    y tenga encuenta todos los valores.

--- TestLogica/test.php
+++ TestLogica/test.php
@@ -15,15 +15,11 @@
         $res2 = 0;
         $suma1 = 0;
         $suma2 = 0;
-        $ant1 = 0;
-        $ant2 = 0;
         $sumatoria1 = 0;
 
 
         $j=intval(trim($j));
           for ($i=1; $res1 <= $j || $res2 <= $j; $i++) {
-              $ant1 = $res1;
-              $ant2 = $res2;
 
               $res1 = $n1 * $i;
               $res2 = $n2 * $i;
@@ -33,8 +29,7 @@
 
               }else {
                 echo "3 * $i = $res1 </br></br>";
-                $suma1 = $ant1 + $res1;
-
+                $suma1 = $res1 + $suma1;
               }
 
               if ($res2 >= $j) {
@@ -42,13 +37,13 @@
 
               }else {
                 echo "5 * $i = $res2 </br></br>";
-                $suma2 = $ant2 + $res2;
+                $suma2 = $res2 + $suma2;
               }
           }
           echo "$suma1 , $suma2 </br>";
           return $suma1+$suma2;
       }
-      echo "prueba para los multiplos de 3 y 5 menores que 10 </br></br>";
+      echo "hola prueba para los multiplos de 3 y 5 menores que 10 </br></br>";
       echo multiplos(10)."</br>";
 
      ?>
